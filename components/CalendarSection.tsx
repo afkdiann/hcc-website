ad7d7b8d56356195842e498e80b963879569790b
afkdiann <dianchengt123@gmail.com>
2026-02-13 09:31:24 -0500

    added links to buttons

--- components/CalendarSection.tsx
+++ components/CalendarSection.tsx
@@ -1,16 +1,19 @@
 import SectionHeader from "@/components/SectionHeader";
 import { ChevronRight } from "lucide-react";
+import Link from "next/link";
 
 function CalendarSection() {
-    return ( 
-        <section className="w-full md:w-1/3 md:mx-4">
-            <SectionHeader header="CALENDAR"/>
-            <div className="my-8 md:mx-4 w-full h-96">
-
-            </div>
-             <button className="bg-white md:mx-4 w-full mt-4 flex justify-center p-2 border-4 border-red-800 text-red-800 hover:text-white hover:bg-red-800 font-bold">ALL EVENTS <ChevronRight /></button>
-        </section>
-     );
+  return (
+    <section className="w-full md:w-1/3 md:mx-4">
+      <SectionHeader header="CALENDAR" />
+      <div className="my-8 md:mx-4 w-full h-96"></div>
+      <Link href="events">
+        <button className="bg-white md:mx-4 w-full mt-4 flex justify-center p-2 border-4 border-red-800 text-red-800 hover:text-white hover:bg-red-800 font-bold">
+          ALL EVENTS <ChevronRight />
+        </button>
+      </Link>
+    </section>
+  );
 }
 
 export default CalendarSection;
--- components/NewsSection.tsx
+++ components/NewsSection.tsx
@@ -1,16 +1,19 @@
 import SectionHeader from "@/components/SectionHeader";
 import { ChevronRight } from "lucide-react";
+import Link from "next/link";
 
 function NewsSection() {
-    return ( 
-        <section className="w-full md:w-1/3 md:mx-4">
-            <SectionHeader header="NEWS"/>
-            <div className="bg-white my-8 md:mx-4 w-full h-96">
-                Hello World!
-            </div>
-            <button className="bg-white md:mx-4 w-full mt-4 flex justify-center p-2 border-4 border-red-800 text-red-800 hover:text-white hover:bg-red-800 font-bold">MORE NEWS <ChevronRight /></button>
-        </section>
-     );
+  return (
+    <section className="w-full md:w-1/3 md:mx-4">
+      <SectionHeader header="NEWS" />
+      <div className="bg-white my-8 md:mx-4 w-full h-96">Hello World!</div>
+      <Link href="/news">
+        <button className="bg-white md:mx-4 w-full mt-4 flex justify-center p-2 border-4 border-red-800 text-red-800 hover:text-white hover:bg-red-800 font-bold">
+          MORE NEWS <ChevronRight />
+        </button>
+      </Link>
+    </section>
+  );
 }
 
 export default NewsSection;
