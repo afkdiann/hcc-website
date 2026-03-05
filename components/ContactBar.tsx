d36a73622f59c58394f1978dfc84325884858462
afkdiann <dianchengt123@gmail.com>
2026-03-05 10:16:30 -0500

    removed unused background images; changed contactbar color to gray

--- components/ContactBar.tsx
+++ components/ContactBar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 function ContactBar() {
 
   return (
-    <div className="hidden md:flex justify-end space-x-8 py-2.5 w-full bg-black bg-[url('/images/searchbar-background.png')] bg-cover bg-center">
+    <div className="hidden md:flex justify-end space-x-8 py-2.5 w-full bg-gray-100 bg-cover bg-center">
       <Link className="text-xs font-bold pr-16" href="/about">CONTACT</Link>
     </div>
   );
