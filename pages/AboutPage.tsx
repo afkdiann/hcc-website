c853cf4b9671e0fbfa2fcb11c874c7fa8b2822b1
afkdiann <dianchengt123@gmail.com>
2026-03-05 10:59:52 -0500

    added text to main section of about page

--- pages/AboutPage.tsx
+++ pages/AboutPage.tsx
@@ -20,7 +20,13 @@ function AboutPage() {
           {isOpen && <DropdownMenu onClose={() => setIsOpen(false)} />}
         </div>
       </header>
-      <main className="h-196"></main>
+      <main className="h-196">
+        <div className="py-8 px-4 md:px-80 space-y-6 flex flex-col items-center text-center">
+          <h1 className="font-bold text-4xl border-b border-gray-400">About</h1>
+          <p>Human-Centered Computing is a research theme at the <a className="text-blue-600" href="https://iacs.stonybrook.edu/index.php" target="_blank" rel="noopener">Institute for Advanced Computational Science (IACS).</a></p>
+          <p>Our research is concerned with studying and enhancing human behavior using computational tools. Here, we take &quot;human behavior&quot; in the broad sense: it covers everyday human behavior in small groups (linguistics, psychology); human behavior and agency in society and its institutions, and across societies and time (sociology, economics, political science, history); and human creativity and the study of human creativity (rhetoric, literature studies, and the arts). Social scientists have long studied the interactions and communications that inform decisions and structure communities. However, as social networks have grown larger and more complex and have increasingly moved &lsquo;online,&rsquo; studying these dynamics requires doing so at scales heretofore inaccessible for researchers, demanding new skills and opening opportunities for new trans-disciplinary collaborations.</p>
+        </div>
+      </main>
       <footer>
         <Footer />
       </footer>
