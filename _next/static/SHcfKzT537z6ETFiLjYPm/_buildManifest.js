9a7fda7fd7b2ad648d66621aaa5ade8266bb7511
afkdiann <dianchengt123@gmail.com>
2026-04-29 16:46:55 -0400

    Updates

--- _next/static/SHcfKzT537z6ETFiLjYPm/_buildManifest.js
+++ _next/static/SHcfKzT537z6ETFiLjYPm/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/hcc-website//_next/:path+",
+        "destination": "/hcc-website/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
