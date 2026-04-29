0d587cf7535dcf8baead59db8b97b62c50199444
afkdiann <dianchengt123@gmail.com>
2026-04-29 14:10:55 -0400

    removed deployment config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/hcc-website',
-  assetPrefix: '/hcc-website/',
+  
 };
 
 export default nextConfig;
